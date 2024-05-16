007f3f5321e57118563c5c02c713c4d76846c315
Nicholas Pipitone <npip99@gmail.com>
2024-05-16 17:19:30 -0400

    m

--- js/article.js
+++ js/article.js
@@ -90,10 +90,10 @@ function loadArticle() {
     if (articleId == "israelpalestine") {
         commentoId = "/"
     }
-    document.getElementById('commento-comments').innerHTML = `
+    /*document.getElementById('commento-comments').innerHTML = `
     <script defer src="https://cdn.commento.io/js/commento.js" data-page-id="${commentoId}"></script>
     <div id="commento"></div>
-    `;
+    `;*/
     if (articleId) {
         const articlePath = `./articles/${articleId}.html`;
         fetch(articlePath)
